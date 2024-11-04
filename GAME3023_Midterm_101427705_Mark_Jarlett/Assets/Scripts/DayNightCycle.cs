798d8f5387d18e79e260cd0775f5885354d8b6f9
Ins3rtEpicName <101427705@georgebrown.ca>
2024-11-04 17:32:45 -0500

    added 24 hour clock to the ui

--- GAME3023_Midterm_101427705_Mark_Jarlett/Assets/Scripts/DayNightCycle.cs
+++ GAME3023_Midterm_101427705_Mark_Jarlett/Assets/Scripts/DayNightCycle.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class DayNightCycle : MonoBehaviour
 {
     [SerializeField] private Light2D sun;
     [SerializeField] private Gradient sunColor;
     [SerializeField] private float dayDurationSeconds;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private float normalizedTime;
 
     public float day = 0;
 
@@ -21,8 +25,12 @@ public class DayNightCycle : MonoBehaviour
     void Update()
     {
         day += Time.deltaTime / dayDurationSeconds;
-        float normalizedTime = day % 1;
-
+        normalizedTime = day % 1;
         sun.color = sunColor.Evaluate(normalizedTime);
+
+        int hour = Mathf.FloorToInt(normalizedTime * 24.0f);
+        int min = Mathf.FloorToInt((normalizedTime - hour / 24.0f) * 1500.0f);
+        string timeString = "Time: " + $"{hour:00}:{min:00}";
+        timeText.text = timeString;
     }
 }
